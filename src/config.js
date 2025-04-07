5bc809d5e6c18a3a05021dd8d68bf02ca38b091c
ChosenCypher <hugs@duck.com>
2025-04-07 20:35:41 +0000

    AGNOS 10

--- src/config.js
+++ src/config.js
@@ -1,8 +1,6 @@
 const config = {
   manifests: {
-    //release: 'https://raw.githubusercontent.com/commaai/openpilot/release3/system/hardware/tici/agnos.json',
-    release: 'https://raw.githubusercontent.com/commaai/openpilot/release3-staging/system/hardware/tici/agnos.json',
-    master: 'https://raw.githubusercontent.com/commaai/openpilot/master/system/hardware/tici/agnos.json',
+    release: 'https://raw.githubusercontent.com/commaai/openpilot/refs/tags/v0.9.7/system/hardware/tici/agnos.json',
   },
   loader: {
     url: 'https://raw.githubusercontent.com/commaai/flash/master/src/QDL/programmer.bin',
